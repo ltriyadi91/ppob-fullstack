12bd87fa91f796641ce581f73f27f8ae95fbfb55
ltriyadi91 <triyadilukman92@gmail.com>
2025-05-27 09:52:49 +0700

    feat(be): liquibase changes related seed data

--- apps/backend/src/main/resources/db/sql/PPOB-SLUG-05.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO slugs (category_id, slug, created_at, updated_at) VALUES
-(1, 'pulsa', NOW(), NOW()),
-(2, 'paket-data', NOW(), NOW()),
-(3, 'listrik', NOW(), NOW());
---
+++ apps/backend/src/main/resources/db/sql/PPOB-TICKER-06.sql
@@ -0,0 +1,2 @@
+INSERT INTO tickers (description, type, category_id, operator_id, is_active, created_at, updated_at) VALUES
+('Sobat PPOB, pembelian dan pembayaran Paket Data untuk produk Telkomsel tidak dapat dilakukan di jam 23:00-00:30 WIB setiap harinya sesuai dengan ketentuan dari Telkomsel.', 'ERROR', 1, 1, true, now(), now());
